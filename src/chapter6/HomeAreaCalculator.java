11a05c1a5b891023257adcf87a379b3ffc5a4a4f
mwaz <amby.mwaz@gmail.com>
2020-02-13 06:55:13 +0300

    fix: add class utilizing base

--- src/chapter6/HomeAreaCalculator.java
+++ src/chapter6/HomeAreaCalculator.java
@@ -2,4 +2,19 @@ package chapter6;
 
 public class HomeAreaCalculator {
 
+		public static void main(String args[]){
+			Rectangle room1 = new Rectangle();
+			room1.setWidth(25);
+			room1.setLength(50);
+			double roomOneArea = room1.calculateArea();
+
+			Rectangle room2 = new Rectangle(30, 75);
+			double roomTwoArea = room2.calculateArea();
+			double totalArea = roomOneArea + roomTwoArea;
+
+			System.out.println("Area of both rooms is " + String.format("%.2f", totalArea));
+
+
+		}
+
 }
