f927ac76054492bb5f1a7f996b50732ffdfac9d5
mwaz <amby.mwaz@gmail.com>
2020-04-16 12:41:03 +0300

    fix: polymorphic animal class

---
+++ src/chapter10/Cat.java
@@ -0,0 +1,15 @@
+package chapter10;
+
+public class Cat extends Animal{
+
+	@Override
+	public void makeSound(){
+		System.out.println("meaow meaow");
+	}
+
+	public void scratch(){
+		System.out.println("I love scratching stuff");
+	}
+
+
+}
